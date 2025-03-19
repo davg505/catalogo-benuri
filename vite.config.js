d296437242831828c5687f508d53e781b7194554
davg505 <david.a.p.g@hotmail.com>
2025-03-19 17:18:49 -0300

    teste11

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Configuração do Vite
 export default defineConfig({
   plugins: [react()],
-  base: "/catalogo-benuri/", // ✅ Apenas UMA VEZ, sem repetições
-});s
+  base: '/catalogo-benuri/',  // A base do projeto no GitHub Pages
+});
